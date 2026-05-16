05e6cab7432e0370d08a276d3c1c902eb7511086
joseph khella <josephkhella2010@gmail.com>
2026-05-16 17:31:03 +0200

    wefhweufbewufwe

--- frontend/train/src/App.tsx
+++ frontend/train/src/App.tsx
@@ -3,10 +3,6 @@ function App() {
     <>
       <h1> hellow world</h1>
       <h1> hi jesus</h1>
-      <h1> how are u</h1>
-      <h1> hellow world</h1>
-      <h1> hi jesus</h1>
-      <h1> how are u</h1>
     </>
   );
 }
