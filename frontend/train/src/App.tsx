020effab3f199c1cdedc0a925b3ebe73aec6b92d
joseph khella <josephkhella2010@gmail.com>
2026-05-16 18:17:07 +0200

    third header

--- frontend/train/src/App.tsx
+++ frontend/train/src/App.tsx
@@ -4,6 +4,10 @@ function App() {
       <h1> hellow world</h1>
       <h1> hi jesus</h1>
       <h1>Jesus love you</h1>
+      <h1>Jesus love you</h1>
+      <h1>Jesus love you</h1>
+      <h1>Jesus love you</h1>
+      <h1>Jesus love you</h1>
     </>
   );
 }
